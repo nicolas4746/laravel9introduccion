67498a31aef577c34dd5c22e7a154b75f0a4da1b
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-10-04 09:48:31 -0300

    add inicio de sesion clase 13 y 14

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PageController;
 
-/**
-Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('blog', [PageController::class, 'blog'])->name('blog');
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
-*/
-
 Route::controller(PageController::class)->group(function () {
 
     Route::get('/',           'home')->name('home');
@@ -17,3 +11,10 @@ Route::controller(PageController::class)->group(function () {
     Route::get('blog/{post:slug}', 'post')->name('post');
 
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
