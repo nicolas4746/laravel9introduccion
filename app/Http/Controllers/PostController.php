675a9186abd4971f44a14fe2674ba03736c5061e
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-10-04 14:42:16 -0300

    clase 18 crear y editar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,22 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Post;
 use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
-    public function index(){
-        return view('posts.index', [
-            'posts' => Post::latest()->paginate(),
+    public function index()
+    {
+    	return view('posts.index', [
+            'posts' => Post::latest()->paginate()
         ]);
     }
 
-    public function destroy(Post $post){
+    public function create()
+    {
+        return view('posts.create');
+    }
 
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function destroy(Post $post)
+    {
         $post->delete();
-
         return back();
     }
 }
