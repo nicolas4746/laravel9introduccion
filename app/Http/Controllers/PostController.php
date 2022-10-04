d7af2a5dacf04ed1f15d058f7946bd95ee9ed502
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-10-04 15:39:03 -0300

    clase 20 funcion de guardar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Http\Controllers;
+
 use App\Models\Post;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 class PostController extends Controller
 {
     public function index()
@@ -10,17 +14,26 @@ class PostController extends Controller
             'posts' => Post::latest()->paginate()
         ]);
     }
-
     public function create(Post $post)
     {
         return view('posts.create', compact('post'));
+    }
+
+    public function store(Request $request)
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug'  => Str::slug($title),
+            'body'  => $request->body,
+        ]);
+
+        return redirect()->route('posts.edit', $post);
     }
 
     public function edit(Post $post)
     {
         return view('posts.edit', compact('post'));
     }
-
     public function destroy(Post $post)
     {
         $post->delete();
