2f830c20453177f1fe9e3c685b2777a1b9f673ec
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-10-04 10:58:42 -0300

    clase 16 listado de publicaciones

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,21 +10,23 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <table class="mb-4">
-                        @foreach($posts as $post)
-                            <tr class="border-b border-gray-200 text-sm">
-                                <td class="px-6 py-4">{{ $post->title }}</td>
-                                <td class="px-6 py-4">
-                                    <a href="" class="text-indigo-600">Editar</a>
-                                </td>
-                                <td class="px-6 py-4">Eliminar</td>
-                            </tr>
+                <table class="nb-4">
+            @forelse ($posts as $post)
+                <tr class="border-b border-gray-200 text-sm">
+                    <td class="px-6 py-4">{{ $post->title }}</td>
+                    <td class="px-6 py-4">
+                        <a href=""class="text-indigo-600" >Editar</a>
+                    </td>
+                    <td class="px-6 py-4"id="">Eliminar</td>
+                </tr>
+            @empty
+                <tr class="border-b border-gray-200 text-sm">
+                    Upps! no hay ninguna publicacion disponible
+                </tr>
+            @endforelse
 
-                        @endforeach
-                        <!-- Les recomiendo hacer uso de la directiva @forelse y no del @foreach. Prácticamente cumplen la misma función, la diferencia radica en que el @forelse me válida si el array viene vacio y me da una opción. Por ejemplo, que el usuario no tenga ninguna publicación (post) que ver, si usaramos el @foreach nos daria error.comentario de Platzi -->
-                    </table>
-                    {{ $posts->links() }}
-                </div>
+        </table>
+        {{ $posts->links() }}
             </div>
         </div>
     </div>
