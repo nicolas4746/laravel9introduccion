47ab6be9e3d4a7e20d930c48302f98b732ce25c3
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-10-05 18:01:11 -0300

    clase 28 y 29 publi ind y buscador

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,12 @@
 @extends('template')
 
 @section('content')
-    <div>
-        <!-- Destacado -->
+    <div class="bg-gray-900 px-20 py-16 rounded-lg mb-8 relative overflow-hidden">
+        <span class="text-xs uppercase text-gray-700 bg-gray-400 rounded-full px-2 py-1">Programación</span>
+        <h1 class="text-3xl text-white mt-4">Blog</h1>
+        <p class="text-sm text-gray-400 mt-2">Proyecto de desarrollo web para profesionales</p>
+
+        <img src="{{ asset('images/dev.png') }}" class="absolute -right-20 -bottom-20 opacity-20">
     </div>
 
     <div class="px-4">
@@ -10,7 +14,7 @@
 
         <div class="grid grid-cols-1 gap-4 mb-4">
             @foreach($posts as $post)
-            <a href="" class="bg-gray-100 rounded-lg px-6 py-4">
+            <a href="{{ route('post', $post->slug) }}" class="bg-gray-100 rounded-lg px-6 py-4">
                 <p class="text-xs flex items-center gap-2">
                     <span class="uppercase text-gray-700 bg-gray-200 rounded-full px-2 py-1">Tutorial</span>
                     <span>{{ $post->created_at->format('d/m/Y') }}</span>
